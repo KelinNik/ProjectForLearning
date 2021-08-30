f14092482891071c552b2a11ebcdf52a84279aec
KelinNik <kelin666@mail.ru>
2021-08-30 22:41:23 +0300

    Исправил сборку

--- module-a/src/main/java/my/learning/project/appa/Application.java
+++ module-a/src/main/java/my/learning/project/appa/Application.java
@@ -2,8 +2,10 @@ package my.learning.project.appa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class Application {
 
     public static void main(String[] args) {
