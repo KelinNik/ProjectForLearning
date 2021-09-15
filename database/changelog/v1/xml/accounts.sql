66380de8adf29d3b1099626e68a8e9e8db415b48
KelinNik <kelin666@mail.ru>
2021-09-15 14:05:27 +0300

    Привёл в соответствие с замечаниями v2.0

--- database/changelog/v1/xml/accounts.sql
+++ database/changelog/v1/xml/accounts.sql
@@ -1,3 +1,5 @@
+--liquibase formatted sql
+--changeset Nick:1 splitStatements:true endDelimiter:;
 create table if not exists account
 (
     account_id  varchar(128) not null,
@@ -7,4 +9,4 @@ create table if not exists account
     constraint acc_id unique (account_id)
 );
 
-create index if not exists account_index on account(account_id)
+create index if not exists account_index on account(account_id);
