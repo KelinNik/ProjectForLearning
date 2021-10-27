588fa180378fb8cda50077b9c428338dc1b149d1
KelinNik <kelin666@mail.ru>
2021-10-27 09:17:18 +0300

    Настроил маппер, удалил класс конфигурации

--- module-b/src/main/java/my/learning/project/appb/mapper/AccountMapper.java
+++ module-b/src/main/java/my/learning/project/appb/mapper/AccountMapper.java
@@ -2,9 +2,10 @@ package my.learning.project.appb.mapper;
 
 import my.learning.project.appb.database.model.AccountModel;
 import my.learning.project.appb.dto.Account;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface AccountMapper {
 
     Account mapToAccount(AccountModel account);
